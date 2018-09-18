9ddf5db37a7b4f181c36ac378245d1fbb217c8c8
Alexei Colin <acolin@isi.edu>
2018-09-18 16:00:13 -0400

    xilinx: apu: support VINITHI output GPIO for CPUs
    VINITHI CPU input controls the configuration of the address
    at which the exception vectors are located at upon reset.

--- hw/misc/xilinx_zynqmp_apu_ctrl.c
+++ hw/misc/xilinx_zynqmp_apu_ctrl.c
@@ -61,6 +61,7 @@
 #define HPSC_TRCH
 
 DEP_REG32(CONFIG0, 0x20)
+    DEP_FIELD(CONFIG0, VINITHI, 4, 8)
 DEP_REG32(RVBARADDR0L, 0x40)
 DEP_REG32(RVBARADDR0H, 0x44)
 DEP_REG32(RVBARADDR1L, 0x48)
@@ -97,9 +98,11 @@ struct ZynqMPAPU {
     qemu_irq wfi_out[4];
     /* CPU Power status towards INTC Redirect. */
     qemu_irq cpu_power_status[4];
+    qemu_irq cpu_vinithi_irqs[4];
 
     uint8_t cpu_pwrdwn_req;
     uint8_t cpu_in_wfi;
+    uint8_t cpu_vinithi;
 
     uint32_t regs[R_MAX];
     DepRegisterInfo regs_info[R_MAX];
@@ -127,7 +130,24 @@ static void zynqmp_apu_reset(DeviceState *dev)
 
     s->cpu_pwrdwn_req = ~0;
     s->cpu_in_wfi = 0;
+    s->cpu_vinithi = 0;
     update_wfi_out(s);
+}
+
+static void zynqmp_apu_config0_post_write(DepRegisterInfo *reg, uint64_t val)
+{
+    ZynqMPAPU *s = ZYNQMP_APU(reg->opaque);
+    unsigned int i, new;
+
+    for (i = 0; i < NUM_CPUS; i++) {
+        new = val & (1 << R_CONFIG0_VINITHI_SHIFT << i);
+        /* Check if CPU's CPUPWRDNREQ has changed. If yes, update GPIOs. */
+        if (new != (s->cpu_vinithi & (1 << i))) {
+            qemu_set_irq(s->cpu_vinithi_irqs[i], !!new);
+        }
+        s->cpu_vinithi &= ~(1 << R_CONFIG0_VINITHI_SHIFT << i);
+        s->cpu_vinithi |= new;
+    }
 }
 
 static void zynqmp_apu_rvbar_post_write(DepRegisterInfo *reg, uint64_t val)
@@ -164,7 +184,10 @@ static void zynqmp_apu_pwrctl_post_write(DepRegisterInfo *reg, uint64_t val)
 }
 
 static const DepRegisterAccessInfo zynqmp_apu_regs_info[] = {
-    { .name = "CONFIG0",  .decode.addr = A_CONFIG0 },
+    { .name = "CONFIG0",  .decode.addr = A_CONFIG0,
+            .reset = 0x0,
+            .post_write = zynqmp_apu_config0_post_write,
+    },
 #ifdef HPSC_TRCH
 #define RVBAR_REGDEF(n) \
     {   .name = "RVBAR CPU " #n " Low",  .decode.addr = A_RVBARADDR ## n ## L, \
@@ -274,6 +297,7 @@ static void zynqmp_apu_init(Object *obj)
     /* CPU_POWER_STATUS is used to connect to INTC redirect. */
     qdev_init_gpio_out_named(DEVICE(obj), s->cpu_power_status,
                              "CPU_POWER_STATUS", 4);
+    qdev_init_gpio_out_named(DEVICE(obj), s->cpu_vinithi_irqs, "VINITHI", 4);
     /* wfi_in is used as input from CPUs as wfi request. */
     qdev_init_gpio_in_named(DEVICE(obj), zynqmp_apu_handle_wfi, "wfi_in", 4);
 }
